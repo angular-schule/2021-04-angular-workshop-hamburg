449949929a530afb64a1b4494a416a9c7aa9143e
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-04-21 15:01:50 +0200

    ngrx final example

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,48 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
 import { BooksRoutingModule } from '../books-routing.module';
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
 import { BookStoreService } from '../shared/book-store.service';
+import { selectBooks, selectBooksLoading } from '../store/book.selectors';
 
 @Component({
   selector: 'br-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent implements OnInit {
 
-  books: Book[] = [];
+  books$ = this.store.pipe(select(selectBooks));
+  loading$ = this.store.pipe(select(selectBooksLoading));
 
-  constructor(
-    private br: BookRatingService,
-    private bs: BookStoreService) {
+  constructor(private store: Store) {
   }
 
   ngOnInit(): void {
-    this.bs.getAll().subscribe(b => this.books = b);
+    // this.bs.getAll().subscribe(b => this.books = b);
   }
 
   doRateDown(book: Book): void {
-    const ratedBook = this.br.rateDown(book);
-    this.updateAndSortBooks(ratedBook);
+    // const ratedBook = this.br.rateDown(book);
+    // this.updateAndSortBooks(ratedBook);
   }
 
   doRateUp(book: Book): void {
-    const ratedBook = this.br.rateUp(book);
-    // const ratedBook = {
-    //   ...book,
-    //   rating: book.rating < 5 ? book.rating + 1 : 5
-    // };
-    this.updateAndSortBooks(ratedBook);
+    // const ratedBook = this.br.rateUp(book);
+    // // const ratedBook = {
+    // //   ...book,
+    // //   rating: book.rating < 5 ? book.rating + 1 : 5
+    // // };
+    // this.updateAndSortBooks(ratedBook);
   }
 
   updateAndSortBooks(ratedBook: Book): void {
-    this.books = this.books
-      .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
-      .sort((a, b) => b.rating - a.rating);
+    // this.books = this.books
+    //   .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
+    //   .sort((a, b) => b.rating - a.rating);
   }
 
   addBook(newBook: Book): void {
-    this.books = [...this.books, newBook];
+    // this.books = [...this.books, newBook];
   }
 }
